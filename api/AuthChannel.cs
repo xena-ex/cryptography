a10b5b985114b17578bfe2423890bcba175bf385
Alexey Semichastnov <asemichastnov@gmail.com>
2018-04-17 11:31:02 +0300

    Trading API .net core authentication sample

---
+++ api/AuthChannel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using GenericWebSocketClient.Channels;
+using XENA.TradingApi.Cryptography;
+
+namespace XENA.TradingApi.Channels
+{
+    public class AuthChannel: BaseChannel
+    {
+        Logon _logon;
+
+        public AuthChannel(List<long> accounts, string apiKeyId, string apiSecret)
+        {
+            _logon = new Logon { Accounts = accounts, Username = apiKeyId };
+
+            var nonce = new DateTimeOffset(DateTime.UtcNow).ToUnixTimeMilliseconds() * 1000000;
+            var authPayload = String.Format("AUTH{0}", nonce);
+            var signature = XENASignature.Sign(apiSecret, authPayload);
+            _logon.SendingTime = nonce;
+            _logon.RawData = authPayload;
+            _logon.Password = signature;
+
+            AwaitableSubscribeResponseMessage = new LoggedOn();
+        }
+
+        public override string Id { get => "auth"; protected set {} }
+
+        public override string Name => "auth";
+
+        public override object SubscribeMessage => _logon;
+
+        public override AwaitableSubscribeResponseMessage AwaitableSubscribeResponseMessage { get; } // TODO
+
+        public override void OnMessage(object msg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ api/Logon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace XENA.TradingApi.Channels
+{
+    public class Logon
+    {        
+        [JsonProperty("35")]
+        public string Message => "A";
+        [JsonProperty("1")]
+        public List<long> Accounts { get; set; }
+        [JsonProperty("553")]
+        public string Username { get; set; }
+        [JsonProperty("52")]
+        public long SendingTime { get; set; }
+        [JsonProperty("96")]
+        public string RawData { get; set; }
+        [JsonProperty("554")]
+        public string Password { get; set; }
+    }
+}
